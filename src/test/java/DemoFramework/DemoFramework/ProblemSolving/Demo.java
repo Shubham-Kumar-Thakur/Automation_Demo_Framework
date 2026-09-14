ccce5c1a9afc6d78b4000d7a612263299e872c5c
ShubhamkumarS <86874879+ShubhamkumarS@users.noreply.github.com>
2026-09-14 10:22:20 +0530

    first commit

---
+++ src/test/java/DemoFramework/DemoFramework/ProblemSolving/Demo.java
@@ -0,0 +1,19 @@
+package DemoFramework.DemoFramework.ProblemSolving;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+//	public static void main(String[] args) {
+//		
+//	}
+
+	@Test
+	public static void WAP_to_print_the_numbers_from_1_to_10_using_for_loop()
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.print(i+" ");
+		}
+	}
+}
